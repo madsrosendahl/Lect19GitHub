21a7d82fc500e879105582911ab7bf10f219f5e6
Mads Rosendahl <98514751+madsrosendahl@users.noreply.github.com>
2024-04-19 16:08:44 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Public Class");
+    System.out.println("Hello Public Record");
     System.out.println("And keep it simple");
   }
 }
